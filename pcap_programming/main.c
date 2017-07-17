289b5bafd0482d5a6d7172b6fea9592074aa5186
Kyeong Joo Jung <iamkj03@naver.com>
2017-07-18 03:37:47 +0900

    MAC address added

--- pcap_programming/main.c
+++ pcap_programming/main.c
@@ -13,68 +13,6 @@
 #include <netinet/ip_icmp.h>
 #include <netinet/udp.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-
-/*
-struct sniff_ip {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    u_int ip_hl:4;
-    u_int ip_v:4;
-#endif
-#if __BYTE_ORDER == __BIG_ENDIAN
-    u_int ip_hl:4;
-    u_int ip_v:4;
-#endif
-    u_int8_t ip_tos;
-    u_short ip_len;
-    u_short ip_id;
-    u_short ip_off;
-#define IP_RF 0x8000
-#define IP_DF 0x4000
-#define IP_MF 0x2000
-#define IP_OFFMASK 0x1fff
-    u_int8_t ip_ttl;
-    u_int8_t ip_p;
-    u_short ip_sum;
-    struct in_addr ip_src,ip_dst;
-};
-
-typedef u_int tcp_seq; //tcp header
-
-struct sniff_tcp{
-    u_int16_t th_sport;
-    u_int16_t th_dport;
-    tcp_seq th_seq;
-    tcp_seq th_ack;
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    u_int8_t th_x2:4;
-    u_int8_t th_off:4;
-#endif
-#if __BYTE_ORDER == __BIG_ENDIAN
-    u_int8_t th_x2:4;
-    u_int8_t th_off:4;
-#endif
-
-    u_int8_t th_flags;
-#define TH_FIN 0x01
-#define TH_SYN 0x02
-#define TH_RST 0x04
-#define TH_PUSH 0x08
-#define TH_ACK 0x10
-#define TH_URG 0x20
-    u_int16_t th_win;
-    u_int16_t th_sum;
-    u_int16_t th_urp;
-};
-
-#define ETHER_ADDR_LEN 6
-struct sniff_ethernet {
-    u_char ether_dhost[ETHER_ADDR_LEN];
-    u_char ether_shost[ETHER_ADDR_LEN];
-    u_short ether_type;
-};
-*/
 
 
 void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
@@ -86,14 +24,27 @@ void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *p
     u_short ether_type;
     int chcnt = 0;
     int length = pkthdr->len;
+    int i = 0;
 
     ep = (struct ether_header *)packet;
 
     packet += sizeof(struct ether_header);
 
     ether_type = ntohs(ep->ether_type);
+
+
+
     if(ether_type == ETHERTYPE_IP)
     {
+        printf("ETHER Source Address = ");
+        for (i = 0; i<ETH_ALEN; ++i)
+            printf("%.2X ", ep->ether_shost[i]);
+        printf("\n");
+        printf("ETHER Destination Address = ");
+        for (i = 0; i<ETH_ALEN;++i)
+            printf("%.2X ", ep->ether_dhost[i]);
+        printf("\n");
+
         iph = (struct ip *)packet;
         printf("IP\n");
         printf("Version :%d\n", iph->ip_v);
